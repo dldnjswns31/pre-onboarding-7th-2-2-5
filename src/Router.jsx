f86eda98d55a89c66fd8b57a66d93c9519c689df
hyein <poptarts@kakao.com>
2022-11-07 01:49:27 +0900

    fix: 충돌 오류 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,27 +1,15 @@
-import React from 'react';
-<<<<<<< HEAD
-import AdAdminPage from './pages/AdAdminPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin" element={<AdAdminPage />} />
-      </Routes>
-    </BrowserRouter>
-=======
 import { Routes, Route } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
 import ManagementPage from './pages/ManagementPage';
+import AdAdminPage from './pages/AdAdminPage';
 
 const Router = () => {
   return (
     <Routes>
+      <Route path="/admin" element={<AdAdminPage />} />
       <Route path="/" element={<DashboardPage />} />
       <Route path="/management" element={<ManagementPage />} />
     </Routes>
->>>>>>> c9cc9e3164cc78b65b650f9aae395d7dfa09ed84
   );
 };
 
